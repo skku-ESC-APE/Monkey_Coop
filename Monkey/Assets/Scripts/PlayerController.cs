2e8af905fd17d4400e75c1594ae971ed75f7c931
ChocolateStrawberryYumYum <simon882@naver.com>
2024-07-31 12:32:30 +0900

    플레이어 이동
    1 원숭이
    2 뱀
    a 왼쪽 이돌
    d 오른쪽 이동

--- Monkey/Assets/Scripts/PlayerController.cs
+++ Monkey/Assets/Scripts/PlayerController.cs
@@ -2,25 +2,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float walkSpeed = 5f;
 
-    // 각 동물의 스케일을 고정
-    private Vector3 monkeyScale = new Vector3(1f, 1f, 1f);
-    private Vector3 snakeScale = new Vector3(2f, 0.1f, 1f);
-    private Vector3 fishScale = new Vector3(1f, 0.1f, 1f);
+    public GameObject monkeyPrefab;
+    public GameObject snakePrefab;
 
-    private Rigidbody2D rb;
-    private CapsuleCollider2D capsuleCollider;
-    private string currentForm = "Monkey";
-    private bool isGrounded;
+    private GameObject currentAnimal;
+    private Animator animator;
+
+    // 크기 조정 변수
+    public Vector3 monkeyScale = new Vector3(1f, 1f, 1f);
+    public Vector3 snakeScale = new Vector3(2f, 0.1f, 1f);
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        capsuleCollider = GetComponent<CapsuleCollider2D>();
-
-        // Rigidbody2D 회전 잠금 설정
-        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        TransformToMonkey(); // 초기 상태는 원숭이
     }
 
     void Update()
@@ -31,18 +27,26 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        float moveInput = Input.GetAxis("Horizontal");
+        float moveInput = 0;
 
-        if (currentForm == "Monkey" || currentForm == "Snake")
+        if (Input.GetKey(KeyCode.A))
         {
-            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+            moveInput = -1;
+            // 왼쪽 방향으로 회전
+            currentAnimal.transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveInput = 1;
+            // 오른쪽 방향으로 회전
+            currentAnimal.transform.rotation = Quaternion.Euler(0, 90, 0);
         }
 
-        if (currentForm == "Fish")
-        {
-            float verticalInput = Input.GetAxis("Vertical");
-            rb.velocity = new Vector2(moveInput * moveSpeed, verticalInput * moveSpeed);
-        }
+        Vector3 move = new Vector3(moveInput * walkSpeed * Time.deltaTime, 0, 0);
+        currentAnimal.transform.position += move;
+
+        bool isWalking = moveInput != 0;
+        animator.SetBool("isWalking", isWalking);
     }
 
     void HandleTransformation()
@@ -51,72 +55,34 @@ public class PlayerController : MonoBehaviour
         {
             TransformToMonkey();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             TransformToSnake();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            TransformToFish();
-        }
     }
 
-    private void TransformToMonkey()
+    void TransformToMonkey()
     {
-        currentForm = "Monkey";
-        transform.localScale = monkeyScale; // 원숭이의 비율로 변경
-        capsuleCollider.size = new Vector2(monkeyScale.x, monkeyScale.y); // 원숭이의 크기에 맞게 조정
-        capsuleCollider.direction = CapsuleDirection2D.Vertical; // 방향 설정
-        rb.velocity = new Vector2(rb.velocity.x, 0); // 원숭이로 변신 시 수직 속도 초기화
-        AdjustToGround(); // 변신 후 바닥에 붙도록 조정
+        if (currentAnimal != null) Destroy(currentAnimal);
+        currentAnimal = Instantiate(monkeyPrefab, transform.position, Quaternion.identity);
+        currentAnimal.transform.localScale = monkeyScale; // 원숭이 크기 조정
+        currentAnimal.transform.position = new Vector3(transform.position.x, 2.0f, transform.position.z); // 원숭이 높이 설정
+        animator = currentAnimal.GetComponent<Animator>();
+        ResetAnimatorParameters();
     }
 
-    private void TransformToSnake()
+    void TransformToSnake()
     {
-        currentForm = "Snake";
-        transform.localScale = snakeScale; // 뱀의 비율로 변경
-        capsuleCollider.size = new Vector2(snakeScale.x, snakeScale.y); // 뱀의 크기에 맞게 조정
-        capsuleCollider.direction = CapsuleDirection2D.Vertical; // 방향 설정
-        rb.velocity = new Vector2(rb.velocity.x, 0); // 뱀으로 변신 시 수직 속도 초기화
-        AdjustToGround(); // 변신 후 바닥에 붙도록 조정
+        if (currentAnimal != null) Destroy(currentAnimal);
+        currentAnimal = Instantiate(snakePrefab, transform.position, Quaternion.identity);
+        currentAnimal.transform.localScale = snakeScale; // 뱀 크기 조정
+        currentAnimal.transform.position = new Vector3(transform.position.x, 1.0f, transform.position.z); // 뱀 높이 설정
+        animator = currentAnimal.GetComponent<Animator>();
+        ResetAnimatorParameters();
     }
 
-    private void TransformToFish()
+    void ResetAnimatorParameters()
     {
-        currentForm = "Fish";
-        transform.localScale = fishScale; // 물고기의 비율로 변경
-        capsuleCollider.size = new Vector2(fishScale.x, fishScale.y); // 물고기의 크기에 맞게 조정
-        capsuleCollider.direction = CapsuleDirection2D.Horizontal; // 물고기는 수평 방향 캡슐
-        rb.velocity = Vector2.zero; // 물고기로 변신 시 속도 초기화
-    }
-
-    private void AdjustToGround()
-    {
-        // 바닥에 붙어 있는 경우에만 위치를 조정
-        if (isGrounded)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, LayerMask.GetMask("Ground"));
-            if (hit.collider != null)
-            {
-                // 플레이어의 캡슐 콜라이더 크기만큼 바닥 위로 이동
-                transform.position = new Vector2(transform.position.x, hit.point.y + capsuleCollider.size.y / 2);
-            }
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
+        animator.SetBool("isWalking", false);
     }
 }
